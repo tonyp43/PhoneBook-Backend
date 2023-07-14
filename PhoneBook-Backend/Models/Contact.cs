7d6c87240b35781fd0d1e22a8d3feb0e6337608b
tonyp43 <tonypavlov@live.com>
2023-07-14 13:29:54 +0200

    Update models

--- PhoneBook-Backend/Models/Contact.cs
+++ PhoneBook-Backend/Models/Contact.cs
@@ -1,6 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
 namespace PhoneBook_Backend.Models;
 
 public class Contact
 {
-    
+    //first name, last name, phone number, email, and social network links.
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string PhoneNumber { get; set; }
+
+    public string Email { get; set; }
+
+    public string SocialNetworkLink { get; set; }
+
+    public string UserId { get; set; }
+    public IdentityUser User { get; set; }
+
 }
--- PhoneBook-Backend/Models/User.cs
+++ PhoneBook-Backend/Models/User.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PhoneBook_Backend.Models;
 
 public class User
 {
-    
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    public string Email { get; set; }
 }
